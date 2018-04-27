03f04a2f8c61970f515e19b8aa6d17d0db722c63
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2018-04-27 14:03:17 +0700

    fix bagian

--- application/controllers/Bagian.php
+++ application/controllers/Bagian.php
@@ -6,7 +6,7 @@ class Bagian extends CI_Controller {
     function __construct()    {
         parent::__construct();
         $this->load->model('M_bagian');
-        $this->load->model('M_pagawai');
+        $this->load->model('M_pegawai');
         if ($this->session->userdata('status_login')!="login") {
             redirect(base_url(''));
         }
@@ -28,7 +28,7 @@ class Bagian extends CI_Controller {
 
     public function tambah(){
         $data = array(
-            'data_pegawai'  => $this->M_pagawai->get_all();
+            'data_pegawai'  => $this->M_pegawai->get_all(),
             'page_title'    => ucwords($this->uri->segment(2)." ".str_replace("_", " ", $this->uri->segment(1))),
         );
         $this->load->view('bagian/v_tambah_bagian',$data);
