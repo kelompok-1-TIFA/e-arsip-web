db9ed1452c61e9a809ed01897e1915bda0d61108
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2018-05-03 09:01:15 +0700

    no message

--- application/controllers/Laporan_surat_masuk.php
+++ application/controllers/Laporan_surat_masuk.php
@@ -5,6 +5,7 @@ class Laporan_surat_masuk extends CI_Controller {
 
     function __construct()    {
         parent::__construct();
+        $this ->load->model('M_surat_masuk');
         if ($this->session->userdata('status_login')!="login") {
             redirect(base_url(''));
         }
@@ -12,12 +13,30 @@ class Laporan_surat_masuk extends CI_Controller {
     }
 
     public function Laporan_harian(){
-      echo "Laporan_harian";
+        $surat_masuk = $this->M_surat_masuk->get_all();
+
+        $data = array(
+            'data_surat_masuk' => $surat_masuk,
+            'page_title'        => ucwords(str_replace("_", " ", $this->uri->segment(1))),
+        );
+        $this->load->view('laporan_surat_masuk/v_laporan_harian_surat_masuk',$data);
     }
-     public function Laporan_bulanan(){
-      echo "Laporan_bulanan";
+    public function Laporan_bulanan(){
+        $surat_masuk = $this->M_surat_masuk->get_all();
+
+        $data = array(
+            'data_surat_masuk' => $surat_masuk,
+            'page_title'        => ucwords(str_replace("_", " ", $this->uri->segment(1))),
+        );
+        $this->load->view('laporan_surat_masuk/v_laporan_bulanan_surat_masuk',$data);
     }
-     public function Laporan_tahunan(){
-      echo "Laporan_tahunan";
+    public function Laporan_tahunan(){
+        $surat_masuk = $this->M_surat_masuk->get_all();
+
+        $data = array(
+            'data_surat_masuk' => $surat_masuk,
+            'page_title'        => ucwords(str_replace("_", " ", $this->uri->segment(1))),
+        );
+        $this->load->view('laporan_surat_masuk/v_laporan_tahunan_surat_masuk',$data);
     }
 }
