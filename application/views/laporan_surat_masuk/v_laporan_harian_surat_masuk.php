3e486efc1f4cf61faa962009cc6462ab5f63c3ab
Zavira Wahyu Dewi Pratiwi <wahyuzavira3@gmail.com>
2018-05-03 11:37:40 +0700

    Laporan Harian Surat Masuk dan Surat Keluar

--- application/views/laporan_surat_masuk/v_laporan_harian_surat_masuk.php
+++ application/views/laporan_surat_masuk/v_laporan_harian_surat_masuk.php
@@ -4,7 +4,52 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-info card-header-icon">
+                            <div class="card-icon">
+                                <i class="material-icons">mail</i>
+                            </div>
+                            <h4 class="card-title">Data</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="toolbar">
+                                <a title="Tambah Data" href="<?php echo base_url('surat_masuk/tambah') ?>" class="btn btn-primary btn-round"><i class="material-icons">add</i> Tambah Data</a>
+                            </div>
+                            <div class="material-datatables">
+                                <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>No Surat</th>
+                                            <th>Asal Surat</th>
+                                            <th>Perihal</th>
+                                            <th>Tanggal Arsip</th>
+                                            <th>File</th>
+                                            <th class="disabled-sorting text-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php $no=0; foreach ($data_surat_masuk as $surat_masuk): ?>
+                                        <tr id="datanya">
+                                            <td><?php echo ++$no; ?></td>
+                                            <td><?php echo $surat_masuk->no_surat ?></td>   
+                                            <td><?php echo $surat_masuk->asal_surat ?></td>
+                                            <td><?php echo $surat_masuk->perihal ?></td>
+                                            <td><?php echo $surat_masuk->tgl_arsip ?></td>
+                                            <td><?php echo $surat_masuk->file ?></td>
+                                            <td class="text-right td-actions">
+                                                <a href="<?php echo base_url('bagian/edit/'.$bagian->id_surat_masuk) ?>" title="Edit" class="btn btn-link btn-warning"><i class="material-icons">mode_edit</i></a>
+                                                <a onclick="deletedata()" title="Hapus" class="btn btn-link btn-danger"><i class="material-icons">close</i></a>
+                                            </td>
+                                        </tr>
+                                        <?php endforeach ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div><!-- end content-->
+                    </div><!--  end card  -->
+                </div> <!-- end col-md-12 -->
             </div>
         </div>
     </div>
