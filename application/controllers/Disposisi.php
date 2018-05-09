ca3368103b6d6e7a39e15fe684bd62e82564fca8
LUTFI AULIA SARI <lutfiauliasari23@gmail.com>
2018-05-09 11:01:28 +0700

    fitur disposisi

--- application/controllers/Disposisi.php
+++ application/controllers/Disposisi.php
@@ -30,14 +30,115 @@ class Disposisi extends CI_Controller {
     }
 
     public function edit($id){
-        # code...
+        $row = $this->M_disposisi->get_by_id($id);
+       
+        if ($row) {
+            $data = array(
+                'id_disposisi'                  => $row->id_disposisi,
+                'id_bagian'                     => $row->id_bagian,
+                'nip'                           => $row->nip,
+                'isi_disposisi'                 => $row->isi_disposisi,
+                'sifat'                         => $row->sifat,
+                'catatan'                       => $row->catatan,
+                'id_surat_masuk'                => $row->id_surat_masuk,
+                'data_bagian'                   => $this->M_bagian->get_all(),
+                'page_title'                    => ucwords($this->uri->segment(2)." ".str_replace("_", " ", $this->uri->segment(1))),
+            );
+            $this->load->view('disposisi/v_disposisi', $data);
+        } else {
+             $this->session->set_flashdata('message', 'swal({
+                title: "Alert",
+                text: "Data Tidak Ditemukan !",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-danger",
+                type: "warning"
+            }).catch(swal.noop)');
+            redirect(site_url('disposisi'));
+
+        }
     }
 
     public function simpan(){
-        # code...
+        $id_disposisi= $this->input->post('id_disposisi');
+        $id_bagian= $this->input->post('id_bagian');
+        $nip= $this->input->post('nip');
+        $isi_disposisi= $this->input->post('isi_disposisi');
+        $sifat= $this->input->post('sifat');
+        $catatan= $this->input->post('catatan');
+        $id_surat_masuk= $this->input->post('id_surat_masuk');
+        $data = array(
+            'id_disposisi'   => "",
+            'id_bagian'      => $id_bagian, 
+            'nip'            => $nip, 
+            'isi_disposisi'  => $isi_disposisi, 
+            'sifat'          => $sifat, 
+            'catatan'        => $catatan, 
+            'id_surat_masuk' => $id_surat_masuk, 
+            );
+        $result = $this->M_disposisi->insert($data);
+        if($result>=0){
+            $this->session->set_flashdata("sukses", 'swal({
+                title: "Berhasi!",
+                text: "Data Berhasil diSimpan!",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-success",
+                type: "success"
+            }).catch(swal.noop)');
+            header('location:'.base_url().'disposisi');
+        }else{
+            $this->session->set_flashdata("alert", 'swal({
+                title: "Gagal!",
+                text: "Data Gagal diSimpan!",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-danger",
+                type: "error"
+            }).catch(swal.noop)');
+            header('location:'.base_url().'disposisi');
+        }
+
+
+        
     }
 
     public function editaction(){
+        $id_disposisi= $this->input->post('id_disposisi');
+        $id_bagian= $this->input->post('id_bagian');
+        $nip= $this->input->post('nip');
+        $isi_disposisi= $this->input->post('isi_disposisi');
+        $sifat= $this->input->post('sifat');
+        $catatan= $this->input->post('catatan');
+        $id_surat_masuk= $this->input->post('id_surat_masuk');
+        $data = array(
+            'id_disposisi'   => $this->input->post('id'),
+            'id_bagian'      => $id_bagian, 
+            'nip'            => $nip, 
+            'isi_disposisi'  => $isi_disposisi, 
+            'sifat'          => $sifat, 
+            'catatan'        => $catatan, 
+            'id_surat_masuk' => $id_surat_masuk, 
+            );
+        $res = $this->M_disposisi->update($data['id_disposisi'],$data);
+        if($res>=0){
+            $this->session->set_flashdata("sukses", 'swal({
+                title: "Berhasi!",
+                text: "Data Berhasil diUpdate!",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-success",
+                type: "success"
+            }).catch(swal.noop)');
+            header('location:'.base_url().'disposisi');
+        }else{
+            $this->session->set_flashdata("alert", 'swal({
+                title: "Gagal!",
+                text: "Data Gagal diUpdate!",
+                buttonsStyling: false,
+                confirmButtonClass: "btn btn-danger",
+                type: "error"
+            }).catch(swal.noop)');
+            header('location:'.base_url().'disposisi');
+             
+    }
+
         # code...
     }
 
@@ -50,6 +151,9 @@ class Disposisi extends CI_Controller {
     }
 
     public function hapus(){
+        $id = $this->input->post("id");
+        $result = $this->M_disposisi->delete($id);
+        header('location:'.base_url().'disposisi'); 
         # code...
     }
 }
--- application/views/disposisi/v_mendisposisikan.php
+++ application/views/disposisi/v_mendisposisikan.php
@@ -16,7 +16,48 @@
                         </div>
                         <form method="POST" action="<?php echo base_url($this->uri->segment(1).'/simpan') ?>" class="form-horizontal" enctype="multipart/form-data">
                               <div class="card-body">
-                            </div>    
+                            </div>
+                            <div class="row">
+                                    <label class="col-md-2 col-form-label font-weight-bold">No Surat</label>
+                                    <div class="col-md-8">
+                                      <div class="form-group">
+                                          <input type="text" name="no_surat" placeholder="" class="form-control" required>
+                                      </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                  <div class="form-group mt-0">
+                                    <select class="selectpicker" data-style="btn select-with-transition" title="Pilih Bagian" data-size="7" name="id_bagian">
+                                        <?php foreach ($data_bagian as $bagian): ?>
+                                            <option value="<?php echo $bagian->id_bagian ?>"> <?php echo $bagian->bagian; ?></option>
+                                        <?php endforeach ?>
+                                    </select>
+                                  </div>
+                                </div>
+                            <div class="row">
+                                    <label class="col-md-2 col-form-label font-weight-bold">Isi Disposisi</label>
+                                    <div class="col-md-8">
+                                      <div class="form-group">
+                                          <textarea name="isi_singkat" placeholder="" class="form-control" required></textarea>
+                                      </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label class="col-md-2 col-form-label font-weight-bold">Sifat</label>
+                                    <div class="col-md-8">
+                                      <div class="form-group">
+                                          <textarea name="isi_singkat" placeholder="" class="form-control" required></textarea>
+                                      </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label class="col-md-2 col-form-label font-weight-bold">Catatan</label>
+                                    <div class="col-md-8">
+                                      <div class="form-group">
+                                          <textarea name="isi_singkat" placeholder="" class="form-control" required></textarea>
+                                      </div>
+                                    </div>
+                                </div>    
                         </form>
                     </div><!--  end card  -->
                 </div> <!-- end col-md-12 -->
