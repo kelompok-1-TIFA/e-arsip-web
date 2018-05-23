e92ef12be132524a6a80a48eb3a455c31dcb6b79
Niko Sebastian <sniko51@yahoo.com>
2018-05-23 19:53:34 +0700

    menu print surat keluat dan masuk. Cek en Yan :v

---
+++ application/views/laporan_surat_keluar/v_laporan_harian_surat_keluar_print.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body onload=" window.print();" onafterprint="window.close();">
+    <table cellpadding="10" border="1" style="border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>No Surat</th>
+                <th>Asal Surat</th>
+                <th>Perihal</th>
+                <th>Tanggal Arsip</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no=0; foreach ($data_surat_keluar as $surat_keluar): ?>
+            <tr>
+                <td><?php echo ++$no; ?></td>
+                <td><?php echo $surat_keluar->no_surat ?></td>   
+                <td><?php echo $surat_keluar->tujuan ?></td>
+                <td><?php echo $surat_keluar->perihal ?></td>
+                <td><?php echo date("d F Y", strtotime($surat_keluar->tgl_arsip)) ?></td>
+            </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</body>
+</html>
