7250d0cb8fe84f581ab0c17a9965e92179a09efd
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2018-07-01 12:17:26 +0700

    no message

--- application/controllers/api/Surat_keluar.php
+++ application/controllers/api/Surat_keluar.php
@@ -10,6 +10,9 @@ class Surat_keluar extends REST_Controller {
     {
         parent::__construct();
         $this->load->model('M_surat_keluar');
+        $this->load->model('M_user');
+        $this->load->model('M_notifikasi');
+        $this->load->model('M_jenis_surat');
     }
 
     function index_get(){
@@ -35,10 +38,95 @@ class Surat_keluar extends REST_Controller {
         }
     }
 
-    /*function index_post(){
-        if ($this->post('api')=="login") {
-            $this->response(['kode' => 1,'pesan' =>'Data berhasil diupdate!'], REST_Controller::HTTP_OK);
+    function index_post(){
+        if ($this->post('api')=="tambah") {
+            $no_surat= $this->post('no_surat');
+            $id_bagian= $this->post('id_bagian');
+            $tujuan=$this->post('tujuan');
+            $isi_singkat=$this->post('isi_singkat');
+            $id_jenis_surat=$this->post('id_jenis_surat');
+            $perihal=$this->post('perihal');
+            $tgl_surat=$this->post('tgl_surat');
+            $keterangan=$this->post('keterangan');
+
+            $jenis_surat=$this->M_jenis_surat->get_by_id($id_jenis_surat);
+
+            $jenis_surat_fix=str_replace(" ", "%20", $jenis_surat->jenis_surat);
+
+            $path='assets/uploads/file/'.$jenis_surat_fix.'/'.'Surat_Keluar_'.$no_surat.'jpeg';
+            if (file_put_contents($path, base64_decode($this->post('file')))) {
+                $data = array(  
+                    'id_surat_keluar'   => "",
+                    'no_surat'          => $no_surat, 
+                    'id_bagian'         => $id_bagian,
+                    'tujuan'            => $tujuan,
+                    'isi_singkat'       => $isi_singkat,
+                    'id_jenis_surat'    => $id_jenis_surat,
+                    'perihal'           => $perihal,
+                    'tgl_surat'         => $tgl_surat,
+                    'tgl_arsip'         => date("Y-m-d"),
+                    'keterangan'        => $keterangan,
+                    'file'              => $path,
+                    'nama_file'         => 'Surat_Keluar_'.$no_surat.'jpeg'
+                );
+
+                $result = $this->M_surat_keluar->insert($data);
+                if($result>=0){
+                    $datauser = $this->M_user->get_where_default("LEFT JOIN tb_pegawai ON tb_pegawai.nip=tb_user.nip_user WHERE id_bagian_pegawai='$data[id_bagian]' or level_user = 'kepala desa'")->result();
+                    $dataterakhir = $this->M_surat_keluar->get_satu_baru();
+                    foreach ($datauser as $user) {
+                        if ($user->level_user=="kepala desa") {
+                            $data_notif = array(
+                                'id_notif'      => "",
+                                'id_user'       => $user->id_user,
+                                'id'            => $dataterakhir->id_surat_keluar,
+                                'jenis_notif'   => "surat keluar",
+                                'judul_notif'   => "Surat Keluar Baru ",
+                                'isi_notif'     => "No. Surat ".$no_surat." Perihal ".$perihal,
+                            );
+                            /*send notif to android*/
+                            $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+                            $msg = array(
+                                'body'  => "No. Surat ".$no_surat." Perihal ".$perihal,
+                                'title' => "Surat Keluar Baru ",
+                                'sound' => 'default'
+                            );
+                            $dt = array(
+                                'id'            => $dataterakhir->id_surat_keluar,
+                                'jenis_notif'   => "surat keluar",
+                            );
+                            $notification = [
+                                "to"  => $user->token,
+                                'notification'      => $msg,
+                                'data'              => $dt
+                            ];
+
+                            $headers = [
+                                'Authorization: key=AAAABYQOPYQ:APA91bFyhxHctHnOWHt54CQXDk9CqwID4twwFuMFwaP3GoTJmhDBfwYT0HUhyEa-8W3szf9wHJbiDG9okjXwB3h1KIoX3eyewsxBw8XNA5_sWWwVCn4CmyPYcSaO7UeQX-KG5EYsB7DA',
+                                'Content-Type: application/json'
+                            ];
+
+                            $ch = curl_init();
+                            curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+                            curl_setopt($ch, CURLOPT_POST, true);
+                            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
+                            $result = curl_exec($ch);
+                            curl_close($ch);
+                            /*send notif to android*/
+                            $this->M_notifikasi->insert($data_notif);    
+                        }
+                    }
+                    $this->response(['kode' => 1,'pesan' =>'Data Berhasil disimpan!'], REST_Controller::HTTP_OK);
+                }else{
+                    $this->response(['kode' => 2,'pesan' =>'Data gagal disimpan!'], REST_Controller::HTTP_OK);
+                }
+            }else{
+                $this->response(['kode' => 2,'pesan' =>'Data gagal disimpan!'], REST_Controller::HTTP_OK);
+            } 
         }
-    }*/
+    }
 
 }
--- application/controllers/api/Surat_masuk.php
+++ application/controllers/api/Surat_masuk.php
@@ -10,6 +10,9 @@ class Surat_masuk extends REST_Controller {
     {
         parent::__construct();
         $this->load->model('M_surat_masuk');
+        $this->load->model('M_user');
+        $this->load->model('M_notifikasi');
+        $this->load->model('M_jenis_surat');
     }
 
     function index_get(){
@@ -27,10 +30,96 @@ class Surat_masuk extends REST_Controller {
         }
     }
 
-    /*function index_post(){
-        if ($this->post('api')=="login") {
-            $this->response(['kode' => 1,'pesan' =>'Data berhasil diupdate!'], REST_Controller::HTTP_OK);
+    function index_post(){
+        if ($this->post('api')=="tambah") {
+            $no_surat= $this->post('no_surat');
+            $asal_surat= $this->post('asal_surat');
+            $isi_singkat=$this->post('isi_singkat');
+            $id_jenis_surat=$this->post('id_jenis_surat');
+            $perihal=$this->post('perihal');
+            $tgl_surat=$this->post('tgl_surat');
+            $keterangan=$this->post('keterangan');
+
+            $jenis_surat=$this->M_jenis_surat->get_by_id($id_jenis_surat);
+
+            $jenis_surat_fix=str_replace(" ", "%20", $jenis_surat->jenis_surat);
+
+            $path='assets/uploads/file/'.$jenis_surat_fix.'/'.'Surat_Masuk_'.$no_surat.'jpeg';
+            if (file_put_contents($path, base64_decode($this->post('file')))) {
+
+                $data = array(  
+                    'id_surat_masuk'    => "",
+                    'no_surat'          => $no_surat, 
+                    'asal_surat'        => $asal_surat,
+                    'isi_singkat'       => $isi_singkat,
+                    'id_jenis_surat'    => $id_jenis_surat,
+                    'perihal'           => $perihal,
+                    'tgl_surat'         => $tgl_surat,
+                    'tgl_arsip'         => date("Y-m-d"),
+                    'keterangan'        => $keterangan,
+                    'file'              => $path,
+                    'nama_file'         => 'Surat_Masuk_'.$no_surat.'jpeg'
+                );
+
+                $result = $this->M_surat_masuk->insert($data);
+                if($result>=0){
+                    $datauser = $this->M_user->get_all();
+                    $dataterakhir = $this->M_surat_masuk->get_satu_baru();
+                    $to1="";
+                    $data_notif = array();
+                    foreach ($datauser as $user) {
+                        if ($user->level_user=="kepala desa") {
+                            $data_notif = array(
+                                'id_notif'      => "",
+                                'id_user'       => $user->id_user,
+                                'id'            => $dataterakhir->id_surat_masuk,
+                                'jenis_notif'   => "surat masuk",
+                                'judul_notif'   => "Surat Masuk Baru ",
+                                'isi_notif'     => "No. Surat ".$no_surat." Perihal ".$perihal,
+                            );
+                            /*send notif to android*/
+                            $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+                            $msg = array(
+                                'body'  => "No. Surat ".$no_surat." Perihal ".$perihal,
+                                'title' => "Surat Masuk Baru ",
+                                'sound' => 'default'
+                            );
+                            $dt = array(
+                                'id'            => $dataterakhir->id_surat_masuk,
+                                'jenis_notif'   => "surat masuk",
+                            );
+                            $notification = [
+                                "to"  => $user->token,
+                                'notification'      => $msg,
+                                'data'              => $dt
+                            ];
+
+                            $headers = [
+                                'Authorization: key=AAAABYQOPYQ:APA91bFyhxHctHnOWHt54CQXDk9CqwID4twwFuMFwaP3GoTJmhDBfwYT0HUhyEa-8W3szf9wHJbiDG9okjXwB3h1KIoX3eyewsxBw8XNA5_sWWwVCn4CmyPYcSaO7UeQX-KG5EYsB7DA',
+                                'Content-Type: application/json'
+                            ];
+
+                            $ch = curl_init();
+                            curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+                            curl_setopt($ch, CURLOPT_POST, true);
+                            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
+                            $result = curl_exec($ch);
+                            curl_close($ch);
+                            /*send notif to android*/
+                            $this->M_notifikasi->insert($data_notif);   
+                        }
+                    }
+                    $this->response(['kode' => 1,'pesan' =>'Data berhasil diSimpan!'], REST_Controller::HTTP_OK);
+                }else{
+                    $this->response(['kode' => 2,'pesan' =>'Data gagal diSimpan!'], REST_Controller::HTTP_OK);
+                }
+            }else{
+                $this->response(['kode' => 2,'pesan' =>'Data gagal diSimpan!'], REST_Controller::HTTP_OK);
+            }
         }
-    }*/
+    }
 
 }
